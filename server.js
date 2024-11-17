2d0f7e7389aca13ceae94ae48aa088b642ff2675
hiwot Tadesse <hiwibesty@gmail.com>
2024-11-17 14:30:56 +0900

    Initial commit: Basic server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 200;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+app.get('/name', (req, res) => {
+  res.send('Hiwot Tadesse');
+});
+
+app.get('/hobby', (req, res) => {
+  res.json({ hobby: 'cooking' });
+});
+
+app.get('/dream', (req, res) => {
+  res.send('Dream big, stay focused, and never give up. Your perseverance will lead to greatness');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
